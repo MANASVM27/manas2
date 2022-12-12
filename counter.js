8625ec7e0c8db13a4322bb05f324abd155875650
MANASVM27 <119093966+MANASVM27@users.noreply.github.com>
2022-12-12 19:42:58 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+const Max1=() =>{
+    return (
+        <>
+        <h1>Hello World</h1>
+        </>
+    )
+  }
+  const Max2=()=>{
+    return(
+        <>
+        <h1>Hello Earth</h1>
+        </>
+    )
+  }
+export {Max1,Max2};
